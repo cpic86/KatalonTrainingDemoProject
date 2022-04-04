303d3fd89b730c4a524b9197f5d982067640f172
Corey Piccioni <Corey Piccioni@DESKTOP-JKG480S.hitronhub.home>
2022-04-04 15:11:53 -0400

    added test suite demo 2

---
+++ Test Suites/Test Suite Demo 2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>2</description>
+   <name>Test Suite Demo 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>7</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>746e13d3-129a-48ed-9eb4-7c02234f85c7</testSuiteGuid>
+   <testCaseLink>
+      <guid>3814e4b1-1242-404d-a4a3-6f6bf4627907</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testing Sample/Record Sample 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
